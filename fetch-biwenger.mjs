b7bf5b1f54d984def720f9ebd14a6055bcb6f0c8
manuelgarcia <manuel.garcia.ext@rsi.cajarural.com>
2026-07-27 14:07:56 +0200

    Fix icons + add to jornadas table

--- fetch-biwenger.mjs
+++ fetch-biwenger.mjs
@@ -63,7 +63,7 @@ async function main() {
     position: s.position,
     name: s.name,
     points: s.points,
-    icon: s.icon ? 'https://cdn.biwenger.com/' + s.icon : null,
+    icon: s.icon ? (s.icon.startsWith('http') ? s.icon : 'https://cdn.biwenger.com/' + s.icon) : null,
     id: s.id,
   }));
 
